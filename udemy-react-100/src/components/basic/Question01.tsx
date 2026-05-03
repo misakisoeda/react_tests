b90ce664ce416cd4d55c284a9566313e56dcd7fa
misakisoeda <misaki.soeda.29@gmail.com>
2026-05-03 17:19:11 +0900

    feat: q1

---
+++ udemy-react-100/src/components/basic/Question01.tsx
@@ -0,0 +1,13 @@
+// const Question01: React.FC = () => {
+//   return (
+//     <h1>Hello, React!</h1>
+//   );
+// }
+
+const Question01 = () => {
+  return (
+    <h1>Hello, React!</h1>
+  );
+}
+
+export default Question01;
